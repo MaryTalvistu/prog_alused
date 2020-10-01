f72267e47fa8e03d7ae76f4f815a051aa7a5c926
Mary Talvistu <marytalvistu@gmail.com>
2020-10-01 11:06:00 +0300

    Ylesande 3.1 lahendus

---
+++ 3/3.1.py
@@ -0,0 +1,10 @@
+kord = int(input("Sisestage mitu korda äratada: "))
+''' 
+for k in range(1, kord+1, 1):
+           print("Tõuse ja sära!")
+           
+'''
+k = 1
+while(k <= kord):
+    print("Tõuse ja sära!")
+    k += 1
