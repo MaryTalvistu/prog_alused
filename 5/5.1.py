7df56bb4f31417f6daff88d9f665f3f188c0e8ba
Mary Talvistu <marytalvistu@gmail.com>
2020-10-08 11:34:36 +0300

    Ylesande 5.1 muudatus

--- 5/5.1.py
+++ 5/5.1.py
@@ -1,29 +1,29 @@
 fail = open("rebased.txt", encoding="UTF-8")
-vastuvõetud = []
+vastuvoetud = []
 for rida in fail:
-     vastuvõetud.append(int(rida))
+     vastuvoetud.append(int(rida))
 fail.close()
 
-print(vastuvõetud)
+print(vastuvoetud)
 aasta = input("Sisestage aasta: ")
 
 if aasta == "2011":
-    arv = vastuvõetud[0]
+    arv = vastuvoetud[0]
 elif aasta == "2012":
-    arv = vastuvõetud[1]
+    arv = vastuvoetud[1]
 elif aasta == "2013":
-    arv = vastuvõetud[2]
+    arv = vastuvoetud[2]
 elif aasta == "2014":
-    arv = vastuvõetud[3]
+    arv = vastuvoetud[3]
 elif aasta == "2015":
-    arv = vastuvõetud[4]
+    arv = vastuvoetud[4]
 elif aasta == "2016":
-    arv = vastuvõetud[5]
+    arv = vastuvoetud[5]
 elif aasta == "2017":
-    arv = vastuvõetud[6]
+    arv = vastuvoetud[6]
 elif aasta == "2018":
-    arv = vastuvõetud[7]
+    arv = vastuvoetud[7]
 else:
-    arv = vastuvõetud[8]
+    arv = vastuvoetud[8]
 
 print(aasta + ". aastal võeti vastu " + str(arv) + " inimest.")
