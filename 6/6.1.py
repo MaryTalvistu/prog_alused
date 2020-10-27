e805f0694a4484b2deb57f1e4f132dbd37a67708
Mary Talvistu <marytalvistu@gmail.com>
2020-10-27 23:13:10 +0200

    Ylesande 6.1 lahendus

---
+++ 6/6.1.py
@@ -0,0 +1,21 @@
+# def banner(x):
+#    return reklaamlause.upper()
+
+#reklaamlause = input("Sisestage reklaamlause");
+#print(banner(reklaamlause))
+
+
+kordi = int(input("Mitu korda soovite reklaamlauset kuvada? "))
+reklaamlause = input("Sisestage reklaamlause: ")
+i= 0;
+
+while i < kordi:
+    print(reklaamlause.upper())
+    i += 1
+
+
+
+
+
+
+
