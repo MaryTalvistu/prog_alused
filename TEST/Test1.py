65bc5ee76251d94f1c4823f8d8011b9418aac16c
Mary Talvistu <marytalvistu@gmail.com>
2020-10-28 17:49:57 +0200

    Testi nr 1 lahendus

---
+++ TEST/Test1.py
@@ -0,0 +1,41 @@
+# jususliku numbri genereerimiseks impordin randint funktsiooni
+from random import randint
+
+# küsin toimumiskohta
+toimumiskoht = input("Kus toimusid maailmameistrivõistlused: ");
+print("Turniirid toimusid " + toimumiskoht)
+
+# küsin toimunud turniiride arvu ja muudan sõne arvuks
+turniiride_koguarv = int(input("Kui palju turniire toimus: "));
+
+# loon kõigepealt tühja turniiride nimekirja ja siis lisan sinna juhusega valitud naiskondade arvud
+# nimekirjas on niipalju elemente kui sisestatud turniiride koguarv
+
+turniiride_nimekiri = []
+i = 0
+while i <= turniiride_koguarv:
+    turniiride_nimekiri.append(randint(10,30))
+    i += 1
+print(turniiride_nimekiri)
+
+
+# võtan ühekaupa nimekirjast naiskondade arvud
+# kui naiskondi rohkem kui 15, siis tugiisikuid 10, muidu 8
+for naiskondade_arv in turniiride_nimekiri:
+    if naiskondade_arv > 15:
+        tugiisikute_arv = 10
+    else:
+        tugiisikute_arv = 8
+
+# defineerin funktsiooni inimeste_arv
+def inimeste_arv(naiskondade_arv, tugiisikute_arv):
+
+    return naiskondade_arv * (22 + tugiisikute_arv)
+
+print("Turniiril oli " + str(naiskondade_arv) + " ja vastavalt inimesi " + str(inimeste_arv()))
+
+
+# arvutan turniiridel osalejate arvu kokku
+print("Kokku oli kõigil turniiridel inimesi: " + str(sum()) )
+
+
